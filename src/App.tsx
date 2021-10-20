fabe5fee36ffdfdd65639730061eeced0a66d237
Ricardo Baltazar <ricardo.baltazar04@gmail.com>
2021-10-20 17:24:45 -0300

    creating project and initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 100vh;
+    background-image: linear-gradient(#FF7F00, #FFBB00);
+	background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    h1 {
+        color: #ffffff;
+        font-size: 2.3rem;
+    }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+	    <Container>
+            <h1>Previsão do tempo</h1>
+        </Container>
+    </>
   );
 }
 
